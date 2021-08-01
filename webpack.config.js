6fe9a52380fc232517e157916b58f60a55711a04
Tirth <tirth089@users.noreply.github.com>
2021-08-01 15:40:37 +0530

    configured webpack, added scss loader and added env variables (#1)

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = env => {
+  return {
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js',
+      publicPath: '/',
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|j?g|svg|gif|ico)?$/,
+          use: 'file-loader'
+        }
+      ]
+    },
+    plugins: [
+      new Dotenv({
+        path: `./.env${env.file ? `.${env.file}` : ''}`
+      }),
+      new HtmlWebPackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        filename: 'index.html',
+        favicon: './public/favicon.ico'
+      })
+    ]
+  }
+};
